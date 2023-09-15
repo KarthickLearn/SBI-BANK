ba7c324ec28b108c3b1409f23fa544a2515a1363
Karthicklearn <Karthickgnc2012@gmail.com>
2023-09-15 14:29:58 +0530

    SBI-BANK created User Details Service

---
+++ User-Details-Service/src/test/java/com/sbi/userdetails/service/UserDetailsService/UserDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbi.userdetails.service.UserDetailsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
